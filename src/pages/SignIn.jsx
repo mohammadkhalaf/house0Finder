373eb174d4ea55744f4dd07167c808d5e06c8f83
mohammad <mohammadkhalaf@live.com>
2022-12-13 10:08:32 +0100

    add register  pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import { MdVisibility } from 'react-icons/md';
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [signinForm, setSigninForm] = useState({
+    userEmail: '',
+    userPassword: '',
+  });
+  const { userEmail, userPassword } = signinForm;
+  const onchangeHandler = (e) => {
+    setSigninForm((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  const showPasswordHandler = () => {
+    setShowPassword(!showPassword);
+  };
+  return (
+    <div>
+      <h2>welcome back</h2>
+      <form>
+        <input
+          type='text'
+          placeholder='Your email'
+          id='userEmail'
+          value={userEmail}
+          onChange={onchangeHandler}
+        />
+        <input
+          type={showPassword ? 'text' : 'password'}
+          id='userPassword'
+          value={userPassword}
+          placeholder='Your password'
+          onChange={onchangeHandler}
+        />
+        <MdVisibility onClick={showPasswordHandler} />
+        <Link to='/forgetpassword'>Forget password?</Link>
+        <button>
+          Sign in <AiOutlineArrowRight />
+        </button>
+      </form>
+      <p>Do not have an account?</p>
+      <Link to='/signup'> Sin up </Link>
+    </div>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import { MdVisibility } from 'react-icons/md';
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [signupForm, setSignupForm] = useState({
+    userEmail: '',
+    userPassword: '',
+    userName: '',
+  });
+  const { userEmail, userPassword, userName } = signupForm;
+  const onchangeHandler = (e) => {
+    setSignupForm((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  const showPasswordHandler = () => {
+    setShowPassword(!showPassword);
+  };
+  return (
+    <div>
+      <h2>welcome back</h2>
+      <form>
+        <input
+          type='text'
+          placeholder='Your Name'
+          id='userName'
+          value={userName}
+          onChange={onchangeHandler}
+        />
+        <input
+          type='text'
+          placeholder='Your email'
+          id='userEmail'
+          value={userEmail}
+          onChange={onchangeHandler}
+        />
+        <input
+          type={showPassword ? 'text' : 'password'}
+          id='userPassword'
+          value={userPassword}
+          placeholder='Your password'
+          onChange={onchangeHandler}
+        />
+        <MdVisibility onClick={showPasswordHandler} />
+        <Link to='/forgetpassword'>Forget password?</Link>
+        <button>
+          Sign Up <AiOutlineArrowRight />
+        </button>
+      </form>
+      <p>You have an account?</p>
+      <Link to='/signin'> Sign in </Link>
+    </div>
+  );
 };
 
 export default SignUp;
