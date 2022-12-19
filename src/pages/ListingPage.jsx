be86c764ea858d1a25baeeac7a838ab132e21bca
mohammad <mohammadkhalaf@live.com>
2022-12-19 16:29:07 +0100

    add slider

--- src/pages/ListingPage.jsx
+++ src/pages/ListingPage.jsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { getDoc, doc } from 'firebase/firestore';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { db, auth } from '../firebase';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const ListingItem = () => {
   const [listing, setListing] = useState(null);
@@ -33,6 +41,35 @@ const ListingItem = () => {
     <>
       {listing && (
         <main>
+          <div>
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              slidesPerView={1}
+              pagination={{ clickable: true }}
+              style={{
+                border: '1px solid green',
+                width: '500px',
+                height: '500px',
+              }}
+            >
+              {listing.imgUrls.map((url, index) => {
+                return (
+                  <SwiperSlide key={index}>
+                    <div
+                      style={{
+                        position: 'relative',
+                        background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                        height: '300px',
+                        width: '300px',
+                        border: '1px solid red',
+                        backgroundSize: 'cover',
+                      }}
+                    ></div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
           <button
             onClick={() => {
               navigator.clipboard.writeText(window.location.href);
@@ -60,10 +97,34 @@ const ListingItem = () => {
             )}
             <p>{listing.bedrooms} bedrooms</p>
             <p>
-              {listing.bathrooms}{' '}
+              {listing.bathrooms}
               {listing.bathrooms > 1 ? 'bathrooms' : 'bathroom'}
             </p>
             <p>{listing.furnished && 'furnished'}</p>
+            <div>
+              <MapContainer
+                style={{
+                  height: '300px',
+                  width: '300px',
+                  border: '1px solid red',
+                }}
+                center={[listing.geolocation.lat, listing.geolocation.long]}
+                zoom={13}
+                scrollWheelZoom={false}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+                />
+
+                <Marker
+                  position={[listing.geolocation.lat, listing.geolocation.long]}
+                >
+                  <Popup>{listing.address}</Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+
             {auth.currentUser?.uid !== listing.userRef && (
               <Link
                 to={`/contact/${listing.userRef}?listingName=${listing.name}`}
